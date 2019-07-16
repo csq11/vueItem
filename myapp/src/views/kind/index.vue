6b7f6bd5441e642261792288a811ff9f99129d71
csq11 <361714757@qq.com>
2019-07-16 09:19:06 +0800

    three

--- myapp/src/views/kind/index.vue
+++ myapp/src/views/kind/index.vue
@@ -1,30 +1,13 @@
 <template>
   <div class="container">
-    <header class="header">分类头部</header>
-    <div class="content">
-     <!--  {{ count }}
-      <button @click="add">加1</button>
-      <Myinput
-        label = "手机号"
-        v-model="username"
-        required
-        clearable
-        placeholder = "请输入您的手机号"
-      />
-      <Myinput
-        label = "密码"
-        v-model="password"
-        placeholder = "请输入您的密码"
-        clearable
-        err="密码格式错误"
-      /> -->
+    <header class="header">团购</header>
+    <div class="content"
       <van-list
         v-model="loading"
         :finished="finished"
         finished-text="没有更多了"
         @load="onLoad"
       >
-      <!-- <img src="https://s11.mogucdn.com/mlcdn/c45406/181123_498kc2f9gdc9if7ke2fe35egahhjl_640x960.jpg" alt=""> -->
         <van-cell
           v-for="item in list"
           :key="item"
